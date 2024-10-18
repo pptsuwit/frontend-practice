b035939d1a8d4bed5951bec7149fb62ef3b207a6
t <pptsuwit@gmail.com>
2024-10-18 19:42:09 +0700

    add practice2

--- src/app/practice-02/page.tsx
+++ src/app/practice-02/page.tsx
@@ -1,5 +1,31 @@
 "use client";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Image } from "antd";
+import { useState } from "react";
 
-export default function Page() {
-  return <div className="grid grid-cols-3 gap-4 p-4"></div>;
+export default function Practice2() {
+  const [items, setItems] = useState(Array.from({ length: 24 }));
+  const fetchMoreData = async () => {
+    setTimeout(() => {
+      setItems((prev) => [...prev, ...Array.from({ length: 12 })]);
+    }, 500);
+  };
+  return (
+    <div>
+      <InfiniteScroll
+        className="grid sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-2 p-4 "
+        dataLength={items.length}
+        next={fetchMoreData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        height={"98vh"}
+      >
+        {items.map((i, index) => (
+          <div className="text-center" key={`image-scroll-${index}-${i}`}>
+            <Image src={`https://picsum.photos/id/${index}/500/500`} alt={`image-${index}`}></Image>
+          </div>
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
 }
